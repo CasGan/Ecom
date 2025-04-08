60303bb2a51d1d09997b2d04c2f5f2c7bc8c22f2
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-04-07 17:59:07 -0700

    First Commit
    skeleton with sign up implemented on the backend with mongodb, redis

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,73 @@
+import { redis } from "../lib/redis.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+
+const generateTokens = (userId) => {
+    const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    });
+
+    const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    });
+
+    return {accessToken, refreshToken};
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+    await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 60 * 60 * 24 * 7); // 7 days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // prevent xss attacks, cross site scripting
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevents CSRF attacks, cross site request forgery
+        maxAge: 15 * 60 * 1000, // 15 minutes   
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // prevent xss attacks, cross site scripting
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevents CSRF attacks, cross site request forgery
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days 
+    });
+}; 
+
+export const signup = async(req, res) => {
+    const { email, password, name } = req.body;
+    try{
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+    
+        const user = await User.create({ name, email, password });
+
+        // authenticate the user
+        const {accessToken, refreshToken} = generateTokens(user._id);     
+        await storeRefreshToken(user._id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({ user:{
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }, message: "User created successfully" });
+
+        
+    }catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const login = async(req, res) => {
+    res.send("Login route called");
+};
+
+export const logout = async(req, res) => {
+    res.send("Logout route called");
+};
