d167204d2bb44e39dcc43f8025747a64fb912142
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-04-15 21:52:42 -0700

    route_handling
    protecte route depending on user, implement logout, login redirection.

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -2,10 +2,10 @@ import { create } from "zustand";
 import axios from "../lib/axios";
 import {toast} from "react-hot-toast";
 
-export const useUserStore = create( (set, get) => ({
+export const useUserStore = create((set, get) => ({
     user:null, 
     loading: false, 
-    checingAuth: true,
+    checkingAuth: true,
 
     signup: async ({name, email, password, confirmPassword}) =>{
         set({loading: true});
@@ -17,7 +17,7 @@ export const useUserStore = create( (set, get) => ({
 
         try{
             const res = await axios.post("/auth/signup", {name, email, password});
-            set({user: res.data.user, loading: false});
+            set({user: res.data, loading: false});
         } catch(error){
             set({loading: false})
             toast.error(error.response.data.message || "An Error Occurred. Try Again.");
@@ -29,13 +29,33 @@ export const useUserStore = create( (set, get) => ({
 
         try {
             const res = await axios.post("/auth/login", {email, password});
-            set({user: res.data.user, loading: false});
+            set({user: res.data, loading: false});
 
         } catch (error) {
             set({loading: false});
             toast.error(error.response.data.message || "An error Occurred");
         }
-    }, 
+    },
 
+    logout: async() =>{
+        try {
+            await axios.post("/auth/logout");
+            set({user: null});
+        } catch (error) {
+            toast.error(error.response?.data?.message || "An error Occured Logging Out");
+        }
+    },
 
+    checkAuth: async() =>{
+        set({checkingAuth: true});
+        try {
+            const response = await axios.get("/auth/profile");
+            set({user:response.data, checkingAuth: false});
+        } catch (error) {
+            console.log(error.message);
+            set({checkingAuth: false, user:null});
+        }
+    },
+
+    //TODO: implement the axios interceptors to refresh access tokens 
 }));
