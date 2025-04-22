e9aab7be1c54534c5469497dc0ef784cfc1aa0b3
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-04-21 20:24:34 -0700

    Cart_page
    initialize people also bought, and order summary, dynamically

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -1,17 +1,18 @@
 import { create } from "zustand";
 import axios from "../lib/axios";
 import { toast } from "react-hot-toast";
-
 export const useCartStore = create((set, get) => ({
   cart: [],
   coupon: null,
   total: 0,
   subtotal: 0,
+  isCouponApplied: false,
 
   getCartItems: async () => {
     try {
       const res = await axios.get("/cart");
       set({ cart: res.data });
+      get().calculateTotals();
     } catch (error) {
       set({ cart: [] });
       toast.error(error.response.data.message || "An Error Occured");
@@ -38,20 +39,47 @@ export const useCartStore = create((set, get) => ({
 
         return { cart: newCart };
       });
+      get().calculateTotals();
     } catch (error) {
       toast.error(error.response.data.message || "An Error Occurred");
     }
   },
 
   calculateTotals: () => {
-    const {cart, coupon} = get(); 
-    const subtotal = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
-    let total = subtotal; 
+    const { cart, coupon } = get();
+    const subtotal = cart.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+    let total = subtotal;
 
-    if(coupon){
-        const discount = subtotal * (coupon.discountPercentage / 100);
-        total = subtotal - discount; 
+    if (coupon) {
+      const discount = subtotal * (coupon.discountPercentage / 100);
+      total = subtotal - discount;
     }
-    set({subtotal, total}); 
-  }
+    set({ subtotal, total });
+  },
+
+  removeFromCart: async (productId) => {
+    await axios.delete(`/cart`, { data: { productId } });
+    set((prevState) => ({
+      cart: prevState.cart.filter((item) => item._id !== productId),
+    }));
+    get().calculateTotals();
+  },
+
+  updateQuantity: async (productId, quantity) => {
+    if (quantity === 0) {
+      get().removeFromCart(productId);
+      return;
+    }
+
+    await axios.put(`/cart/${productId}`, { quantity });
+    set((prevState) => ({
+      cart: prevState.cart.map((item) =>
+        item._id === productId ? { ...item, quantity } : item
+      ),
+    }));
+    get().calculateTotals();
+  },
 }));
